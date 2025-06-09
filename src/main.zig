7a386e1f90c1c7457ec25f7ae523aadf39484d16
xGihyun <giordnnuz@gmail.com>
2025-06-09 12:02:43 +0800

    Revert "add some print statements on `main.zig`"
    This reverts commit 4092b100cae80fd8ed197a448561a8eecea0ed33.

--- src/main.zig
+++ src/main.zig
@@ -1,7 +1,11 @@
 const std = @import("std");
 
 pub fn main() !void {
-    std.debug.print("Don't forget to include an input.txt file on each day!\n", .{});
-    std.debug.print("To get started, run the command:\n\n", .{});
-    std.debug.print("zig build run -- <year> <day> <part>\n", .{});
+    // stdout is for the actual output of your application, for example if you
+    // are implementing gzip, then only the compressed bytes should be sent to
+    // stdout, not any debugging messages.
+    const stdout_file = std.io.getStdOut().writer();
+    var bw = std.io.bufferedWriter(stdout_file);
+
+    try bw.flush(); // don't forget to flush!
 }
